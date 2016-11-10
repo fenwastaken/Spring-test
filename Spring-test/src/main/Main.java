c973d08c85d986bab5e92c1defbcc089fc104c78
fenwastaken <fenwastaken@fenwastaken-pc>
2016-11-10 12:13:00 +0100

    test patate over

--- Spring-test/src/main/Main.java
+++ Spring-test/src/main/Main.java
@@ -12,10 +12,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld obj = (HelloWorld)context.getBean("helloWorld");
-		obj.sayMessage();
-		
-		
+		HelloWorld objA = (HelloWorld)context.getBean("helloWorld");
+		objA.sayMessage();
 	}
 
 }
